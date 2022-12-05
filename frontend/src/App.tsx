4b89672a1ed1754f1e8d4c3d3242b1195d66f137
tristan <tristan@valhalla.team>
2022-12-05 21:45:21 +0800

    Add styled components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,12 @@
+import styled from 'styled-components';
+
+const Root = styled.h1`
+  font-size: 1.5em;
+  color: palevioletred;
+`;
+
 function App() {
-  return <h1 className="text-3xl underline">Hello world!</h1>;
+  return <Root>Hello</Root>;
 }
 
 export default App;
